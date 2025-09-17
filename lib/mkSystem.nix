1ba49caeac2052ee444d3b27015cad59394d12ef
Seth Messer <seth@megalithic.io>
2025-09-17 11:17:10 -0400

    wip(nix): fix that condition again

--- lib/mkSystem.nix
+++ lib/mkSystem.nix
@@ -41,7 +41,7 @@ systemFunc rec {
     # the overlays are available globally.
     { nixpkgs.overlays = overlays; }
 
-    inputs.agenix.packages.${system}.default
+    # inputs.agenix.packages."${system}".default
 
     # Allow unfree packages.
     { nixpkgs.config.allowUnfree = true; }
