beedc0925293b304a3650595e242f4602a39853b
Seth Messer <seth@megalithic.io>
2025-09-19 16:22:16 -0400

    wip(nix): continuing the refactor.

--- users/seth/config/yazi/default.nix
+++ users/seth/config/yazi/default.nix
@@ -1,102 +1,100 @@
 { pkgs, ... }:
 {
-  yazi = {
-    enable = true;
-    enableFishIntegration = true;
-    enableZshIntegration = true;
-    settings = {
-      manager = {
-        ratio = [
-          1
-          3
-          4
-        ];
-        sort_by = "natural";
-        sort_dir_first = true;
-        show_hidden = false;
-        show_symlink = false;
-        linemode = "size";
-      };
-      opener = {
-        edit = [
-          {
-            run = ''nvim "$@"'';
-            block = true;
-          }
-        ];
-        # play = [
-        #   {
-        #     run = ''mpv "$@"'';
-        #     orphan = true;
-        #     for = "unix";
-        #   }
-        # ];
-        open = [
-          {
-            run = ''xdg-open "$@"'';
-            desc = "open";
-          }
-        ];
-      };
-      # open = {
-      #   prepend_rules = [
-      #     {
-      #       name = "*.ts";
-      #       use = "edit";
-      #     }
-      #   ];
-      # };
+  enable = true;
+  enableFishIntegration = true;
+  enableZshIntegration = true;
+  settings = {
+    manager = {
+      ratio = [
+        1
+        3
+        4
+      ];
+      sort_by = "natural";
+      sort_dir_first = true;
+      show_hidden = false;
+      show_symlink = false;
+      linemode = "size";
     };
-
-    keymap = {
-      input = {
-        prepend_keymap = [
-          # https://yazi-rs.github.io/docs/tips#close-input-by-esc
-          {
-            on = [ "<Esc>" ];
-            run = "close";
-            desc = "Cancel input";
-          }
-        ];
-      };
-      manager = {
-        prepend_keymap = [
-          # https://yazi-rs.github.io/docs/tips#dropping-to-shell
-          {
-            on = [ "<C-s>" ];
-            run = "shell ${pkgs.fish} --block --confirm";
-            desc = "Open default shell here";
-          }
-
-          # https://yazi-rs.github.io/docs/tips#smart-enter
-          # also needs smart-enter plugin, below
-          {
-            on = [ "<Enter>" ];
-            run = "plugin --sync smart-enter";
-            desc = "Enter the child directory, or open the file";
-          }
-
-          # {
-          #   on = [
-          #     "f"
-          #     "g"
-          #   ];
-          #   run = "plugin fg";
-          #   desc = "find file by content";
-          # }
-          # {
-          #   on = [
-          #     "f"
-          #     "f"
-          #   ];
-          #   run = "plugin fg --args='fzf'";
-          #   desc = "find file by file name";
-          # }
-        ];
-      };
+    opener = {
+      edit = [
+        {
+          run = ''nvim "$@"'';
+          block = true;
+        }
+      ];
+      # play = [
+      #   {
+      #     run = ''mpv "$@"'';
+      #     orphan = true;
+      #     for = "unix";
+      #   }
+      # ];
+      open = [
+        {
+          run = ''xdg-open "$@"'';
+          desc = "open";
+        }
+      ];
     };
-    plugins = {
-      "smart-enter" = ./plugins/smart-enter;
+    # open = {
+    #   prepend_rules = [
+    #     {
+    #       name = "*.ts";
+    #       use = "edit";
+    #     }
+    #   ];
+    # };
+  };
+
+  keymap = {
+    input = {
+      prepend_keymap = [
+        # https://yazi-rs.github.io/docs/tips#close-input-by-esc
+        {
+          on = [ "<Esc>" ];
+          run = "close";
+          desc = "Cancel input";
+        }
+      ];
     };
+    manager = {
+      prepend_keymap = [
+        # https://yazi-rs.github.io/docs/tips#dropping-to-shell
+        {
+          on = [ "<C-s>" ];
+          run = "shell ${pkgs.fish} --block --confirm";
+          desc = "Open default shell here";
+        }
+
+        # https://yazi-rs.github.io/docs/tips#smart-enter
+        # also needs smart-enter plugin, below
+        {
+          on = [ "<Enter>" ];
+          run = "plugin --sync smart-enter";
+          desc = "Enter the child directory, or open the file";
+        }
+
+        # {
+        #   on = [
+        #     "f"
+        #     "g"
+        #   ];
+        #   run = "plugin fg";
+        #   desc = "find file by content";
+        # }
+        # {
+        #   on = [
+        #     "f"
+        #     "f"
+        #   ];
+        #   run = "plugin fg --args='fzf'";
+        #   desc = "find file by file name";
+        # }
+      ];
+    };
+  };
+  plugins = {
+    "smart-enter" = ./plugins/smart-enter;
   };
 }
