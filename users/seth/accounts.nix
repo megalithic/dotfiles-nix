b32bfc8b9a8d38c7c82b1df76d1fb06000393ca6
Seth Messer <seth@megalithic.io>
2025-09-19 15:53:01 -0400

    wip(nix): continuing the refactor.

--- users/seth/accounts.nix
+++ users/seth/accounts.nix
@@ -1,81 +1,79 @@
 { pkgs, config, ... }: {
-  accounts = {
-    email = {
-      maildirBasePath = "${config.home.homeDirectory}/.mail";
-      accounts = {
-        gmail = {
-          primary = false;
+  email = {
+    maildirBasePath = "${config.home.homeDirectory}/.mail";
+    accounts = {
+      gmail = {
+        primary = false;
 
-          realName = "Seth Messer";
-          address = "seth.messer@gmail.com";
+        realName = "Seth Messer";
+        address = "seth.messer@gmail.com";
 
-          userName = "seth.messer@gmail.com";
-          passwordCommand = "op read op://Shared/aw6tbw4va5bpnippcdqh2mkfq4/Passwd";
+        userName = "seth.messer@gmail.com";
+        passwordCommand = "op read op://Shared/aw6tbw4va5bpnippcdqh2mkfq4/Passwd";
 
-          folders = { inbox = "INBOX"; sent = "\[Gmail\]/Sent\ Mail"; trash = "\[Gmail\]/Trash"; };
-          flavor = "gmail.com";
+        folders = { inbox = "INBOX"; sent = "\[Gmail\]/Sent\ Mail"; trash = "\[Gmail\]/Trash"; };
+        flavor = "gmail.com";
 
-          aerc.enable = true;
-          himalaya.enable = true;
+        aerc.enable = true;
+        himalaya.enable = true;
+      };
+
+      fastmail = {
+        primary = true;
+
+        realName = "Seth Messer";
+        address = "seth@megalithic.io";
+
+        userName = "seth.messer@fastmail.com";
+        passwordCommand = "op read op://Shared/Fastmail/apps/tui";
+        imap = {
+          host = "imap.fastmail.com";
+          tls.enable = true;
         };
 
-        fastmail = {
-          primary = true;
-
-          realName = "Seth Messer";
-          address = "seth@megalithic.io";
-
-          userName = "seth.messer@fastmail.com";
-          passwordCommand = "op read op://Shared/Fastmail/apps/tui";
-          imap = {
-            host = "imap.fastmail.com";
-            tls.enable = true;
+        aerc.enable = true;
+        himalaya.enable = true;
+        notmuch.enable = true;
+        mbsync = {
+          extraConfig.channel = {
+            CopyArrivalDate = "yes";
           };
+          enable = true;
+          create = "both";
+          expunge = "both";
+          remove = "both";
+          # expunge = "none";
+          # remove = "none";
+        };
+      };
 
-          aerc.enable = true;
-          himalaya.enable = true;
-          notmuch.enable = true;
-          mbsync = {
-            extraConfig.channel = {
-              CopyArrivalDate = "yes";
-            };
-            enable = true;
-            create = "both";
-            expunge = "both";
-            remove = "both";
-            # expunge = "none";
-            # remove = "none";
-          };
+      nibuild = {
+        primary = false;
+
+        address = "seth@nibuild.com";
+        realName = "Seth Messer";
+
+        userName = "seth@nibuild.com";
+        passwordCommand = "op read op://Shared/nibuild/password";
+
+        imap = {
+          host = "mail.nibuild.com";
+          tls.enable = true;
         };
 
-        nibuild = {
-          primary = false;
-
-          address = "seth@nibuild.com";
-          realName = "Seth Messer";
-
-          userName = "seth@nibuild.com";
-          passwordCommand = "op read op://Shared/nibuild/password";
-
-          imap = {
-            host = "mail.nibuild.com";
-            tls.enable = true;
+        aerc.enable = true;
+        himalaya.enable = true;
+        notmuch.enable = true;
+        mbsync = {
+          extraConfig.channel = {
+            CopyArrivalDate = "yes";
           };
-
-          aerc.enable = true;
-          himalaya.enable = true;
-          notmuch.enable = true;
-          mbsync = {
-            extraConfig.channel = {
-              CopyArrivalDate = "yes";
-            };
-            enable = true;
-            create = "both";
-            expunge = "both";
-            remove = "both";
-            # expunge = "none";
-            # remove = "none";
-          };
+          enable = true;
+          create = "both";
+          expunge = "both";
+          remove = "both";
+          # expunge = "none";
+          # remove = "none";
         };
       };
     };
